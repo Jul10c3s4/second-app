100fc3bb75d6cd6d62ddcbf235dd5aa311bca77c
julio cesar <jcso2@aluno.ifal.edu.br>
2023-08-05 17:35:57 -0300

    new changes

--- src/pages/Home/tarefaApp.jsx
+++ src/pages/Home/tarefaApp.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Card } from '../../components/card';
 import './styles.css';
-
+import listImage from '../../assets/listas.png';
 export const TarefaApp = () => {
   const [input, setInput] = useState('');
   const [tasks, setTasks] = useState(
@@ -32,7 +32,10 @@ export const TarefaApp = () => {
   return (
     <div className="tarefaApp">
       <div className="container">
-        <h1>Gerenciador de tarefas</h1>
+        <div className="title">
+          <h1>Gerenciador de tarefas</h1>
+          <img src={listImage} />
+        </div>
         <input
           value={input}
           onChange={(e) => {
